ff6c59f0839975600185fd6b9f3f6b030fddad5f
hong yuseok <jericoh@naver.com>
2022-02-18 11:39:24 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Security OAuth 2.0
+#### Spring Security + OAuth 2.0
---
+++ src/main/java/com/SpringSecurityOAuth/SpringSecurityOAuth/global/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.SpringSecurityOAuth.SpringSecurityOAuth.global.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/test/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
